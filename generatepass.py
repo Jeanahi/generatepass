5f5ab54ddfcb7813596a821f1e642b46167552f9
Jeanahi <117412366+Jeanahi@users.noreply.github.com>
2022-11-24 19:41:00 -0300

    generate pass2
    Ejercicio completo de generacion de claves con Python

--- generatepass.py
+++ generatepass.py
@@ -1,8 +1,19 @@
 from tkinter import *
 import random 
+import string
 
 def generatePass():
-    new_label=Label(root,text="hello").pack()
+    lst=[]
+    for i in range(1,13):
+        alphabet_lowers=list(string.ascii_lowercase)
+        alphabet_upper=list(string.ascii_uppercase)
+        alphabet_symbol=list(string.punctuation)
+        str=alphabet_lowers+alphabet_upper+alphabet_symbol
+        character=random.choice(str)
+        lst.append(character)
+    a="".join(lst)
+    print(a)
+    label.config(text=a)  
 
 
 root = Tk()
@@ -17,7 +28,10 @@ root.resizable(0,0)
 my_button=Button(root, text="Generar",font="arial 15 bold ",command=generatePass).pack() 
 my_label=Label(root,text="").pack()
 exit_button=Button(root, text="Salir", font="arial 15 bold",command=root.destroy).pack()
-
-
+my_label=Label(root,text="").pack()
+label_et=Label(root,text="Esta es la contraseña : ")
+label_et.pack()
+label=Label(root,text="",font="arial 15 bold ")
+label.pack()
 
 root.mainloop()
